0f806d0672b7d1824e87d8d17f7575a5a08ae317
Darshan Neshabalan <omgitzdarshan@gmail.com>
2022-10-14 05:05:09 +1100

    Start of Settings Page

--- pages/settings.vue
+++ pages/settings.vue
@@ -13,18 +13,81 @@
         </v-card>
       </v-col>
     </v-row>
-
-    <v-row>
-      <v-col class="text-center">
-        <blockquote class="blockquote">
-          This section is for Darshan
-          <footer>
-            <small>
-              <em>&mdash;Jason</em>
-            </small>
-          </footer>
-        </blockquote>
-      </v-col>
+    <div><h1>Profile Settings</h1></div>
+    <br>
+    <v-row allign="center" justify="center">
+      <div id ="personalSettingsPage">
+        <v-form ref="form" v-model="valid">
+          <v-text-field
+              v-model="fullnameSettings"
+              label="Full name"
+            ></v-text-field>
+            <v-text-field
+              v-model="emailSettings"
+              label="Email"
+            ></v-text-field>
+            <v-text-field
+              v-model="passwordSettings"
+              label="Password"
+              required
+            ></v-text-field>            
+            <v-menu
+              ref="menu"
+              v-model="birthdaySettingsMenu"
+              :close-on-content-click="false"
+              :return-value.sync="date"
+              transition="scale-transition"
+              offset-y
+              min-width="auto"
+            >
+            <template v-slot:activator="{ on, attrs }">
+              <v-text-field
+                v-model="date"
+                label="Birthday"
+                prepend-icon="mdi-calendar"
+                readonly
+                v-bind="attrs"
+                v-on="on"
+              ></v-text-field>
+            </template>
+            <v-date-picker
+              v-model="date"
+              no-title
+              scrollable
+            >
+              <v-spacer></v-spacer>
+              <v-btn
+                text
+                color="primary"
+                @click="menu = false"
+              >
+                Cancel
+              </v-btn>
+              <v-btn
+                text
+                color="primary"
+                @click="$refs.menu.save(date)"
+              >
+                OK
+              </v-btn>
+            </v-date-picker>
+          </v-menu>
+          <v-btn
+            color="success"
+            class="mr-4"
+            @click="saveSettingChanges"
+          >
+            Save Changes
+          </v-btn>
+          <v-btn
+            color="error"
+            class="mr-4"
+            @click="resetSettingChanges"
+          >
+            Cancel
+          </v-btn>
+        </v-form>
+      </div>
     </v-row>
   </v-container>
 </template>
