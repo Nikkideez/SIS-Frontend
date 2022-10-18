f80f10714154d33fdfce6eca21d5c8c5346e522c
Jason Immanuel <63634201+JasonI136@users.noreply.github.com>
2022-10-18 15:30:34 +1100

    Fixes to chart js colours and data

--- components/Calendar/PieChart/PieChart.vue
+++ components/Calendar/PieChart/PieChart.vue
@@ -7,24 +7,28 @@
 <script>
 import Chart from 'chart.js/auto';
 
+
 export default {
+
     data: () => ({
         chart: null,
-
         chartData: {
             type: "doughnut",
             data: {
-                labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
+                labels: ['Sleep', 'Fitness', 'Work', 'Leisure', 'Education'],
                 datasets: [
                     {
                         label: 'Data One',
                         backgroundColor: [
-                            'rgb(255, 99, 132)',
-                            'rgb(54, 162, 235)',
-                            'rgb(255, 205, 86)'
+                            'rgba(255, 99, 132, 1)',
+                            'rgba(54, 162, 235, 1)',
+                            'rgba(255, 206, 86, 1)',
+                            'rgba(75, 192, 192, 1)',
+                            'rgba(153, 102, 255, 1)',
+                            'rgba(255, 159, 64, 1)'
                         ],
                         borderColor: 'rgb(255, 255, 255)',
-                        data: [40, 20, 12, 39, 10, 40, 39]
+                        data: [40, 20, 12, 39, 10]
                     }
                 ]
             }
@@ -32,11 +36,6 @@ export default {
         options: {
             responsive: true,
             maintainAspectRatio: false,
-            plugins: {
-                legend: {
-                    display: false
-                }
-            }
         }
     }),
     mounted() {
