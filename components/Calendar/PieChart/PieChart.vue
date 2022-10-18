082414329c217f0ce6d36ae078eccc5290b51e17
Jason Immanuel <jason.immanuel136@gmail.com>
2022-10-18 23:57:03 +1100

    Pie Chart fully functional

--- components/Calendar/PieChart/PieChart.vue
+++ components/Calendar/PieChart/PieChart.vue
@@ -1,56 +1,117 @@
 <template>
-    <div>
-        <canvas id="myChart"></canvas>
-    </div>
+  <div>
+    <canvas id="myChart"></canvas>
+  </div>
 </template>
-  
+
 <script>
 import Chart from 'chart.js/auto';
 
 
 export default {
 
-    data: () => ({
-        chart: null,
-        chartData: {
-            type: "doughnut",
-            data: {
-                labels: ['Sleep', 'Fitness', 'Work', 'Leisure', 'Education'],
-                datasets: [
-                    {
-                        label: 'Data One',
-                        backgroundColor: [
-                            'rgba(255, 99, 132, 1)',
-                            'rgba(54, 162, 235, 1)',
-                            'rgba(255, 206, 86, 1)',
-                            'rgba(75, 192, 192, 1)',
-                            'rgba(153, 102, 255, 1)',
-                            'rgba(255, 159, 64, 1)'
-                        ],
-                        borderColor: 'rgb(255, 255, 255)',
-                        data: [40, 20, 12, 39, 10]
-                    }
-                ]
-            }
-        },
-        options: {
-            responsive: true,
-            maintainAspectRatio: false,
-        }
-    }),
-    props: {
-        events: Array
+  data: () => ({
+    chart: null,
+    chartData: {
+      type: "doughnut",
+      data: {
+        labels: [],
+        datasets: [
+          {
+            label: 'Data One',
+            backgroundColor: [
+              'rgba(255, 99, 132, 1)',
+              'rgba(54, 162, 235, 1)',
+              'rgba(255, 206, 86, 1)',
+              'rgba(75, 192, 192, 1)',
+              'rgba(153, 102, 255, 1)',
+              'rgba(255, 159, 64, 1)'
+            ],
+            borderColor: 'rgb(255, 255, 255)',
+            data: []
+          }
+        ]
+      }
     },
-    mounted() {
-        const ctx = document.getElementById('myChart');
-        this.chart = new Chart(ctx, this.chartData);
-    },
-    methods: {
-        recalculateTable() {
-            this.chart.update();
-            console.log(this.chart);
-        }
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
     }
+  }),
+
+  props: {
+    events: Array
+  },
+  computed: {
+    calendar: {
+      get() {
+        return this.$store.state.calendar;
+      },
+    }
+  },
+  watch: {
+    calendar(val) {
+      this.getEventsPerWeek();
+    },
+    events: {
+      handler(val) {
+        this.getEventsPerWeek();
+      },
+      deep: true
+    }
+
+
+  },
+  mounted() {
+    const ctx = document.getElementById('myChart');
+    this.chart = new Chart(ctx, this.chartData);
+  },
+  created() {
+    this.getEventsPerWeek();
+  },
+  methods: {
+    recalculateTable() {
+      this.chart.update();
+      console.log(this.chart);
+
+    },
+
+    getEventsPerWeek() {
+      const startWeek = this.$moment(this.calendar, "YYYY-MM-DD").startOf('isoWeek').valueOf()
+      const endWeek = this.$moment(this.calendar, "YYYY-MM-DD").endOf('isoWeek').valueOf()
+      const eventsCurrentWeek = this.events.filter(x => x.start >= startWeek && x.start <= endWeek)
+
+      let result = eventsCurrentWeek.reduce(function (r, a) {
+        r[a.category] = r[a.category] || [];
+        r[a.category].push(a);
+        return r;
+      }, Object.create(null));
+
+
+      var ids = Object.keys(result);
+      this.chartData.data.labels = ids
+
+      let eventData = [];
+      console.log(result);
+      ids.forEach(element => {
+        var count = 0;
+        for(let i = 0; i < result[element].length; i++){
+          count += result[element][i].end - result[element][i].start
+
+        }
+        let hours = (count / (1000 * 60 * 60)).toFixed(1);
+        eventData.push(hours);
+      });
+
+      this.chartData.data.datasets[0].data = eventData;
+
+      // loop all elements within ids
+      // loop all the events within ids
+      if(this.chart){
+        this.recalculateTable();
+      }
+      return ids
+    }
+  }
 }
 </script>
-  
